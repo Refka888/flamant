56ea2e77a76636fb5f283db36bdbcb15198eaf00
Refka <refka.gmati@gmail.com>
2022-09-05 17:34:10 +0100

    remise à zero

--- src/Controller/UserController.php
+++ src/Controller/UserController.php
@@ -2,27 +2,28 @@
 
 namespace App\Controller;
 
-use App\Entity\Users;
-use App\Repository\UsersRepository;
+use App\Entity\User;
+use App\Repository\UserRepository;
+use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 use Symfony\Component\HttpFoundation\JsonResponse;
 use Symfony\Component\Serializer\SerializerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
-use Symfony\Component\HttpFoundation\Response;
 
 class UserController extends AbstractController
 {
     #[Route('/api/users', name: 'user', methods: ['GET'])]
-    public function getAllUsers(UsersRepository $usersRepository, SerializerInterface $serializer): JsonResponse
+    public function getUserList(UserRepository $userRepository, SerializerInterface $serializer): JsonResponse
     {
-        $userList = $usersRepository->findAll();
-        $jsonUserList = $serializer->serialize($userList, 'json');
-        return new JsonResponse($jsonUserList, Response::HTTP_OK, [], true);
+        $userList = $userRepository->findAll();
+        $jsonBookList = $serializer->serialize($userList, 'json');
+        return new JsonResponse($jsonBookList, Response::HTTP_OK, [], true);
     }
-    #[Route('/api/users/{id}', name: 'detailUser', methods: ['GET'])]
-    public function getDetailUsers(Users $user, SerializerInterface $serializer){
-            $jsonUser = $serializer->serialize($user, 'json');
-            return new JsonResponse($jsonUser, Response::HTTP_OK,[], true);
-    } 
     
+    #[Route('/api/users/{id}', name: 'detailUser', methods: ['GET'])]
+    public function getDetailBook(User $user, SerializerInterface $serializer): JsonResponse 
+    {
+        $jsonUser = $serializer->serialize($user, 'json');
+        return new JsonResponse($jsonUser, Response::HTTP_OK, ['accept' => 'json'], true);
+    }
 }
--- src/DataFixtures/AppFixtures.php
+++ src/DataFixtures/AppFixtures.php
@@ -2,10 +2,13 @@
 
 namespace App\DataFixtures;
 
-use App\Entity\Orders;
-use App\Entity\Users;
-use Doctrine\Bundle\FixturesBundle\Fixture;
+
+use App\Entity\User;
+use App\Entity\Order;
 use Doctrine\Persistence\ObjectManager;
+use Doctrine\Bundle\FixturesBundle\Fixture;
+
+
 
 class AppFixtures extends Fixture
 {
@@ -13,8 +16,8 @@ class AppFixtures extends Fixture
     {
         $listOrder = [];
         for ($i=0; $i < 20; $i++){
-            $order = new Orders();
-            $order ->setCode("Code". $i);
+            $order = new Order();
+            $order ->setCode("123456". $i);
             $order ->setStatut("Statut". $i);
             $manager ->persist($order);
             $listOrder[] = $order;
@@ -22,11 +25,11 @@ class AppFixtures extends Fixture
          }
 
      for ($i=0; $i < 20; $i++){
-        $user = new Users();
+        $user = new User();
         $user ->setfirstName("firstName". $i);
         $user ->setlastName("lastName". $i);
         $user ->setemail("user". $i ."@gmail.com");
-        $user ->setPassword("**********". $i);
+        $user ->setpassword("**********". $i);
         $user ->setOrders($listOrder[array_rand($listOrder)]);
         $manager ->persist($user);
 
